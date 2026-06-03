90b07d697e808fb6e6d118c8311bdb62670cd78c
StartSoft <college@gmail.com>
2026-06-03 21:27:07 +0700

    create task modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import FilterList from './FilterList';
 // import FocusInput from './FocusInput';
 
-import React, { useState, useMemo, useEffecct } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { generateMockData } from "./mocKData";
 import DashboardMetrics from "./DashboadMetrics";
 import FilterPanel from "./FilterPanel";
+import AnaliticTable from "./AnalyticTable";
+import TaskModal from "./TaskModal";
 
 function App() {
 
@@ -15,12 +17,35 @@ function App() {
   //   {id: 4, name: 'Alisa'},
   // ]
 
-  const [rawData, setRawData] = useState(()=>generateMockData());
+  const [rawData, setRawData] = useState(() => generateMockData());
   const [filters, setFilters] =useState({search: '', category: '', status: ''});
+  const [isModalOpen, setModalOpen] = useState(false);
   
+  // фильтрация массива
   const filteredData = useMemo(() => {
-
+    return rawData.filter(item => {
+      const matchesSearch = item.title.toLowerCase().includes(filters.search.toLowerCase());
+      const matchesCategory = filters.category ? item.category === filters.category : true;
+      const matchesStatus = filters.status ? item.status === filters.status : true;
+      return matchesSearch && matchesCategory && matchesStatus;
+    })
   }, [rawData, filters.search, filters.category, filters.status]);
+
+  // слушатель горячих клавиш
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.altKey && (e.key === 'n' || e.key === 'т' || e.key === 'N' || e.key === 'Т')){
+        e.preventDefault();
+        setModalOpen(true);
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleAddNewItem = (newItem) => {
+    setRawData(prev => [newItem, ...prev]);
+  };
 
   return (
     // <>
@@ -29,8 +54,22 @@ function App() {
     // </>
     
     <>
-      <DashboardMetrics filteredData={rawData}/>
-      {/* <FilterPanel filters={} setFilters={} onOpenModal={}/> */}
+      <h1>Dashboard Builder</h1>
+      {/* компонент метрик */}
+      <DashboardMetrics filteredData={filteredData}/>
+      {/* панель фильтров */}
+      <FilterPanel  
+        filters={filters}
+        setFilters={setFilters}
+        onOpenModal={() => setModalOpen(true)}  
+      />
+      <AnaliticTable data={filteredData}/>
+      {/* модальное окно */}
+      <TaskModal 
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        onAdd={handleAddNewItem}
+      />
     </>
   );
 };
